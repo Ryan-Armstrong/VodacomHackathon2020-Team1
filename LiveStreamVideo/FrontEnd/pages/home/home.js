2ad9546e3861ca1823948f28a1b9b804dbdc3e68
Kobus <kobus@fastcomm.co.za>
2020-08-12 15:48:24 +0200

    Initial code commit with working scroller

---
+++ LiveStreamVideo/FrontEnd/pages/home/home.js
@@ -0,0 +1,6 @@
+Page({
+  data: {
+      arr: ['Clothing', 'Technolofy', 'Home', 'Accessories', 'Fitness', 'Pets', 'Travel', 'Kitchen', 'Gardening'],
+  },
+  onLoad() {},
+});
--- LiveStreamVideo/FrontEnd/pages/index/index.js
+++ LiveStreamVideo/FrontEnd/pages/index/index.js
@@ -1,8 +1,7 @@
 const app = getApp();
 
 Page({
-  
-  
+
  onLoad(query) {
     // Page loading
     this.webViewContext = my.createWebViewContext('web-view-1');   
