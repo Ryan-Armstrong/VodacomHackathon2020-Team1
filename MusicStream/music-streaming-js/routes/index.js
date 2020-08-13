34e4f2c01ffea58aec6eb6359e6ce6ff55dd290a
stehanvisser <stehan19@gmail.com>
2020-08-13 13:53:45 +0200

    init handelbars and installed libs

--- MusicStream/music-streaming-js/routes/index.js
+++ MusicStream/music-streaming-js/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    condition: false,
+    arr: [1, 2, 3]
+  });
 });
 
 module.exports = router;
