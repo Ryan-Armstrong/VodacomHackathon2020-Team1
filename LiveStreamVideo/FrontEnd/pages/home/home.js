d268bbaae1e1c5c9408da06fb288d4024937d2f2
Kobus <kobus@fastcomm.co.za>
2020-08-13 12:06:30 +0200

    Added icons and center icons

--- LiveStreamVideo/FrontEnd/pages/home/home.js
+++ LiveStreamVideo/FrontEnd/pages/home/home.js
@@ -1,9 +1,17 @@
 Page({
   data: {
-      arr: ['Clothing', 'Technology', 'Home', 'Accessories', 'Fitness', 'Pets', 'Travel', 'Kitchen', 'Gardening'],
+      categoriesArr: ['Clothing', 'Technology', 'Home', 'Accessories', 'Fitness', 'Pets', 'Travel', 'Kitchen', 'Gardening'],
+      channelsArr: ['Juni', 'FTN', 'Vodacom', 'Blu Betty', 'Takealot', 'Frankees', 'The Shine', 'DealDay', 'HomeDepot'],
+      videoForYouArr: ['Lola\'s new collection', 'Nike Pastel Dreams', 'GoGo Mini Bags', 'Blu Betty', 'Takealot', 'Frankees', 'The Shine', 'DealDay', 'HomeDepot'],
   },
   onLoad() {},
   selectCategory(ev) {
+    console.log(ev)
+  },
+  selectChannel(ev) {
+    console.log(ev)
+  },
+  selectVideo(ev) {
     console.log(ev)
   }
 });
