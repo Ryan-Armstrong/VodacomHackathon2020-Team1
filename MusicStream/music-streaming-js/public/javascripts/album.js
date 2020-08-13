ac6587ad8bd95812e357e93e892d0ec291ec81b6
stehanvisser <stehan19@gmail.com>
2020-08-13 23:13:32 +0200

    added music progressbar

--- MusicStream/music-streaming-js/public/javascripts/album.js
+++ MusicStream/music-streaming-js/public/javascripts/album.js
@@ -1,17 +1,44 @@
 var playing;
+var width = 1;
+var duration;
+var timer = 0;
+var myVar;
 
 function togglePlaying() {
-  var audioPlayer = document.getElementById("audio-player");
 
+
+  var audioPlayer = document.getElementById("audio-player");
+  duration = audioPlayer.duration;
+  console.log('duration', audioPlayer.duration);
+  console.log("currentTime", audioPlayer.currentTime);
   console.log("AUDIOPLAYER", audioPlayer);
 
   if (playing) {
     audioPlayer.pause();
     playing = false;
+    clearInterval(myVar);
     document.getElementById("toggleBtn").src = "/icons/play_filled.svg";
   } else {
     audioPlayer.play();
     document.getElementById("toggleBtn").src = "/icons/pause_filled.svg";
     playing = true;
+    clearInterval(myVar);
+    myVar = setInterval(myTimer, 1000);
   }
+}
+
+function updateBar() {
+  var element = document.getElementById("myprogressBar");
+
+  if (timer <= duration) {
+    element.style.width = (timer + 0.25) / duration * 100 + '%';
+  }
+}
+
+function myTimer() {
+  if (playing === true) {
+    timer++;
+    console.log("timer", timer);
+  }
+
 }
