b26e9ca64c8320201b6a68446021784354097e42
nicferretti <ferrettinicholas@gmail.com>
2020-08-14 11:18:22 +0200

    added album sections

--- MusicStream/FrontEnd/app.js
+++ MusicStream/FrontEnd/app.js
@@ -4,7 +4,7 @@ App({
       id:"1",
       title: "Cardigan",
       releaseDate: "03/03/2020",
-      thumbnail: "new_release_1.png",
+      thumbnail: "new-release-thumbnails/new_release_1.png",
       artistName: "Taylor Swift, Folklore",
       songs: [
         {
@@ -17,7 +17,7 @@ App({
       id:"2",
       title: "Easy",
       releaseDate: "08/07/2020",
-      thumbnail: "new_release_2.png",
+      thumbnail: "new-release-thumbnails/new_release_2.png",
       artistName: "Troye Sivan",
       songs: [
         {
@@ -30,12 +30,96 @@ App({
       id:"3",
       title: "Stupid Love",
       releaseDate: "01/07/2020",
-      thumbnail: "new_release_3.png",
+      thumbnail: "new-release-thumbnails/new_release_3.png",
       artistName: "Lady Gaga",
       songs: [
         {
           id:"3",
           title: "Easy"
+        }
+      ]
+    }
+  ],
+
+  recommendedAlbums: [
+    {
+      id:"4",
+      title: "After Hours",
+      releaseDate: "03/03/2020",
+      thumbnail: "recommended-thumbnails/recommended_1.png",
+      artistName: "The Weekend",
+      songs: [
+        {
+          id:"4",
+          title: "After Hours"
+        }
+      ]
+    },
+    {
+      id:"5",
+      title: "Dance Monkey",
+      releaseDate: "08/07/2020",
+      thumbnail: "recommended-thumbnails/recommended_2.png",
+      artistName: "Tones and I",
+      songs: [
+        {
+          id:"5",
+          title: "Dance Monkey"
+        }
+      ]
+    },
+    {
+      id:"6",
+      title: "Fine Line",
+      releaseDate: "01/07/2020",
+      thumbnail: "recommended-thumbnails/recommended_3.png",
+      artistName: "Harry Styles",
+      songs: [
+        {
+          id:"6",
+          title: "Fine Line"
+        }
+      ]
+    }
+  ],
+
+  popularAlbums: [
+    {
+      id:"7",
+      title: "Empini",
+      releaseDate: "03/03/2020",
+      thumbnail: "popular-thumbnails/popular_1.png",
+      artistName: "Kelly Khumalo, Empini",
+      songs: [
+        {
+          id:"7",
+          title: "Empini"
+        }
+      ]
+    },
+    {
+      id:"8",
+      title: "We Made It",
+      releaseDate: "08/07/2020",
+      thumbnail: "popular-thumbnails/popular_2.png",
+      artistName: "Mi Casa",
+      songs: [
+        {
+          id:"8",
+          title: "Eve"
+        }
+      ]
+    },
+    {
+      id:"9",
+      title: "Mortal Man",
+      releaseDate: "01/07/2020",
+      thumbnail: "popular-thumbnails/popular_3.png",
+      artistName: "Jeremy Loops",
+      songs: [
+        {
+          id:"9",
+          title: "Mortal Man"
         }
       ]
     }
--- MusicStream/FrontEnd/pages/music-landing-page/music-landing-page.js
+++ MusicStream/FrontEnd/pages/music-landing-page/music-landing-page.js
@@ -3,7 +3,9 @@ const app = getApp();
 Page({
   data: {
     bannerAds: ['banner_1', 'banner_2', 'banner_3'],
-    newReleases: app.newReleases
+    newReleases: app.newReleases,
+    recommendedAlbums: app.recommendedAlbums,
+    popularAlbums: app.popularAlbums
   },
   onLoad() {
     console.log("Music landing page loaded.")
