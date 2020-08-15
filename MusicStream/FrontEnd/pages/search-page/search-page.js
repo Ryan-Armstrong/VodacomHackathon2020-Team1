d1f1a978080f5f3ad2d508c6547560775ec1e248
Kobus <kobus@fastcomm.co.za>
2020-08-15 17:01:36 +0200

    Search now goes to the correct song that is played

--- MusicStream/FrontEnd/pages/search-page/search-page.js
+++ MusicStream/FrontEnd/pages/search-page/search-page.js
@@ -1,11 +1,12 @@
+const app = getApp();
 Page({
   data: {
     inputValue: '',
-    searchArr: [{ type: "Album", name: "Chromatica", info1: "Lady Gaga", info2: "16 tracks", info3: "01:04:27" }, 
-    { type: "Track", name: "Eve", info1: "Mi Casa", info2: "We Made It"}, 
-    { type: "Artist", name: "The Weekend", info1: "52 Tracks", info2: "3 Albums" }, 
-    { type: "Track", name: "Truth Hurts", info1: "Lizzo", info2: "Truth Hurts"}, 
-    { type: "Artist", name: "Kelly Khumalo", info1: "24 Tracks"},]
+    searchArr: [{ type: "Album", name: "Chromatica", info1: "Lady Gaga", info2: "16 tracks", info3: "01:04:27", id:"3" },
+    { type: "Track", name: "Eve", info1: "Mi Casa", info2: "We Made It", id:"8" },
+    { type: "Artist", name: "The Weekend", info1: "52 Tracks", info2: "3 Albums", id:"4" },
+    { type: "Artist", name: "Kelly Khumalo", info1: "24 Tracks", id:"7" },
+    { type: "Track", name: "Truth Hurts", info1: "Lizzo", info2: "Truth Hurts", id:"7" },]
   },
   onLoad() { },
   onBlur(e) {
@@ -13,7 +14,10 @@ Page({
       inputValue: e.detail.value,
     });
   },
-  loadSong() {
-    my.navigateTo({ url: '../index/index' });
+  loadSong(e) {
+    app.selectedItem = e.target.dataset.id;
+    my.navigateTo({
+      url: '../album-view/album-view'
+    });
   }
 });
