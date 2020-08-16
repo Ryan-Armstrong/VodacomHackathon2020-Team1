f41bbf545c0ca20475523ecb0a7f84e4d161c08e
Kobus <kobus@fastcomm.co.za>
2020-08-16 12:50:55 +0200

    Centered text of best in SA

--- VideoStream/FrontEnd/pages/video-landing-page/video-landing-page.js
+++ VideoStream/FrontEnd/pages/video-landing-page/video-landing-page.js
@@ -12,7 +12,7 @@ Page({
         secondColour: "#e60302"
       }, 
       {
-        name: "Best in South Africa",
+        name: "Best in \n South Africa",
         firstColour: "#f3e65b",
         secondColour: "#26a23d"
       }, 
