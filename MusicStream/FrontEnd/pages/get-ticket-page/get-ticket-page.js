1b337695974642513dbaa903a0f2efc1597e416a
stehanvisser <stehan19@gmail.com>
2020-08-17 11:25:31 +0200

    working ticket view

--- MusicStream/FrontEnd/pages/get-ticket-page/get-ticket-page.js
+++ MusicStream/FrontEnd/pages/get-ticket-page/get-ticket-page.js
@@ -1,12 +1,42 @@
 import ext from '../../extensions.js';
+ var selectedItem = {};
 
 Page({
-  data: {},
-  onLoad() { },
+  data: {
+     items: [
+      {name: '250 MB (2hours) for R7', value: '250'},
+      {name: '500 MB (4hours) for R15', value: '500', checked: true},
+      {name: '1 GB (8hours) for R25', value: '1'},   
+    ],
+  },
+  onLoad() {
+    selectedItem = this.data.items[0].name;
+
+   },
+
   purchaseTicket(e) {
+    let confirmMessage = 'will be added to your August statement for this purchase.';
+
+    switch (selectedItem) {
+      case '250': confirmMessage = 'R7 ' + confirmMessage;
+        
+        break;
+
+        case '500': confirmMessage = 'R15 ' + confirmMessage;
+        
+        break;
+
+        case '1': confirmMessage = 'R25 ' + confirmMessage;
+        
+        break;
+    
+      default: confirmMessage = 'R15 ' + confirmMessage;
+        break;
+    }
+
     my.confirm({
       title: 'Confirm Purchase',
-      content: 'R15 will be added to your August statement for this purchase.',
+      content: confirmMessage,
       confirmButtonText: 'Confirm',
       cancelButtonText: 'Cancel',
       success: (result) => {
@@ -31,6 +61,10 @@ Page({
   onReady()
   {
     ext.setNav();
-  }
+  },
+
+  radioChange(e) {
+    selectedItem = e.detail.value
+  },
 
 });
